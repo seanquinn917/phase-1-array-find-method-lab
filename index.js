66d33b5bfc6e5164602c16f39240b37ddcddaddb
SeanQuinn <Seanquinn917@gmail.com>
2023-01-13 12:55:46 -0500

    commit tree 8ff75d4f91d3bc9ee29fc0dc6f05509e49e58ae6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// function superbowlWin (array){
+//     for(let item of array){
+//         if(item.result === "W"){
+//             return item.year
+//         }
+//    }
+// }
+
+function superbowlWin(record){
+let obj = record.find((item)=>{
+    if(item.result === "W"){
+        return item
+    }
+})
+  if(obj){
+
+   return obj.year}
+}
+
+
